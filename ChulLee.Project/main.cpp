896f8d55c0c60a71c0d51f2125075c07586fb294
ChulLee <chullee.ict@gmail.com>
2021-07-02 13:36:04 +0700

    commit 2 - chul lee

--- ChulLee.Project/main.cpp
+++ ChulLee.Project/main.cpp
@@ -13,6 +13,7 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     cout << "Them de commit lan 1\n";
+    cout << "Chul Lee code dong 2\n";
     return 0;
     
 }
