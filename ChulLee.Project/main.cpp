52a6e5d887702095cba9884cd47f19168c3bf2f5
ChulLee <chullee.ict@gmail.com>
2021-07-02 13:26:04 +0700

    Initial Commit

---
+++ ChulLee.Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ChulLee.Project
+//
+//  Created by Lan Chu on 7/2/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
