d5e43686434960f3430c029b4b1aeee9653ad775
ChulLee <chullee.ict@gmail.com>
2021-07-02 13:27:39 +0700

    Commit 1 - Them cout

--- ChulLee.Project/main.cpp
+++ ChulLee.Project/main.cpp
@@ -7,8 +7,12 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    cout << "Them de commit lan 1\n";
     return 0;
+    
 }
